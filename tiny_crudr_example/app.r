b018cfff9623ec8d3973ab1dcbe2f8dbb5c85f3e
eau leaf <eau.leaf@gmail.com>
2023-03-12 22:24:51 -0700

    to  9013

--- tiny_crudr_example/app.r
+++ tiny_crudr_example/app.r
@@ -1,13 +1,13 @@
 # Minimal crudr App Example
 
-# TODO: Can the 'text_out' code be combined into a single listener?
-# TODO: Can the deltas table updater be combined into a single function?
+# TODO: (important) fix the timezone issue
 # TODO: Add F2 to JS user control
 # TODO: (important) alert user to table change, or do an automatic update given changes
 # someone else has made a change in the DB since you've been updating, do you want to reload?
+# TODO: (important) set up postgres db and test
+# TODO: Can the 'text_out' code be combined into a single listener?
+# TODO: Can the deltas table updater be combined into a single function?
 # TODO: update whether to export objects
-# TODO: (important) update db_tbl name
-# TODO: (important) add some DT changes to user-view table
 
 
 # crudr's goal is to:
@@ -38,12 +38,11 @@
 # create a connection to a database called 'iris.db'
 con <- pool::dbPool(DBI::dbConnect(RSQLite::SQLite(), 'iris.db'))
 
-# paste('Your Database is saved here:', here::here('iris.db'))
+
 # if the datatable doesn't exist, go make it; otherwise, say it's already there
 # Dataset must have a unique identifier column and unique identifiers must be characters in the dataset
 IRIS <- tibble::tibble(dplyr::mutate(iris, day = lubridate::today(), time = lubridate::now()))
-
-cdr_create_tbls_in_db(db_conn_pool = con, db_tbl = IRIS)
+crudr::cdr_create_tbls_in_db(db_conn_pool = con, db_tbl = IRIS)
 # what tables are in the 'iris' database?
 # paste(pool::dbListTables(con), collapse = ', ')
 # if you want to delete the tables later, run these lines. (Does not delete the database; just the tables)
@@ -51,6 +50,8 @@ cdr_create_tbls_in_db(db_conn_pool = con, db_tbl = IRIS)
 # crudr::cdr_remove_tbl(db_conn_pool = con, db_tbl = 'iris_DELTAS')
 # pool::dbRemoveTable(con,'IRIS')
 # pool::dbRemoveTable(con,'IRIS_DELTAS')
+# pool::dbRemoveTable(con,'iris')
+# pool::dbRemoveTable(con,'iris_DELTAS')
 # pool::poolClose(con)
 ##############################################
 
@@ -92,7 +93,7 @@ server <- function(input, output, session){
   #   3) The Changes Log table: is your record of changes to the Editable table. You get the log table through crudr::cdr_admin_tbl_ui('IRIS', 'chg_log_tbl')
 
   iris_r_tbl <- crudr::cdr_manage_db_tbls(
-    id = 'IRIS',
+    table_name = 'IRIS',
     key_col = 'UID',
     db_conn_pool = con,
     session = session,
@@ -104,7 +105,7 @@ server <- function(input, output, session){
 
   output$iris_db_data <- DT::renderDT(
     DT::formatDate(table   = DT::datatable(iris_r_tbl()),
-                   columns = 'WHEN_EDITED_LAST',
+                   columns = c('time', 'WHEN_EDITED_LAST'),
                    method  = 'toLocaleString')
   )
 
